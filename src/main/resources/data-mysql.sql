fe016e07aec901a713cafdc6ad68130cb05da3af
vladthedrummer <45808188+vladthedrummer@users.noreply.github.com>
2020-12-04 20:58:19 -0600

    update
    forgot to change my data.sql to data-mysql.sql

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,52 @@
+/*
+delete from Rank;
+delete from Rank_Name;
+delete from User;
+
+
+insert into Rank(id, name, type)
+values ('UR','Unranked','UNRATED');
+insert into Rank(id, name, type)
+values ('I1', 'Iron One', 'IRON');
+insert into Rank(id, name, type)
+values ('I2', 'Iron Two', 'IRON');
+insert into Rank(id, name, type)
+values('I3', 'Iron Three', 'IRON');
+insert into Rank(id, name, type)
+values ( 'B1', 'Bronze One', 'BRONZE' );
+insert into Rank(id, name, type)
+values ( 'B2', 'Bronze Two', 'BRONZE' );
+insert into Rank(id, name, type)
+values ( 'B3', 'Bronze Three', 'BRONZE' );
+insert into Rank(id, name, type)
+values ( 'S1', 'Silver One', 'SILVER' );
+insert into Rank(id, name, type)
+values ( 'S2', 'Silver Two', 'SILVER' );
+insert into Rank(id, name, type)
+values ( 'S3', 'Silver Three', 'SILVER' );
+insert into Rank(id, name, type)
+values ( 'G1', 'Gold One', 'GOLD' );
+insert into Rank(id, name, type)
+values ( 'G2', 'Gold Two', 'GOLD' );
+insert into Rank(id, name, type)
+values ( 'G3', 'Gold Three', 'GOLD' );
+insert into Rank(id, name, type)
+values ( 'P1', 'Plat One', 'PLATINUM' );
+insert into Rank(id, name, type)
+values ( 'P2', 'Plat Two', 'PLATINUM' );
+insert into Rank(id, name, type)
+values ( 'P3', 'Plat Three', 'PLATINUM' );
+insert into Rank(id, name, type)
+values ( 'D1', 'Diamond One', 'DIAMOND' );
+insert into Rank(id, name, type)
+values ( 'D2', 'Diamond Two', 'DIAMOND' );
+insert into Rank(id, name, type)
+values ( 'D3', 'Diamond Three', 'DIAMOND' );
+insert into Rank(id, name, type)
+values ( 'IM1', 'Immortal One', 'IMMORTAL' );
+insert into Rank(id, name, type)
+values ( 'IM2', 'Immortal Two', 'IMMORTAL' );
+insert into Rank(id, name, type)
+values ( 'IM3', 'Immortal Three', 'IMMORTAL' );
+insert into Rank(id, name, type)
+values ( 'Rad', 'Radiant', 'RADIANT' );*/
